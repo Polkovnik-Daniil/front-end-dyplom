b134fc75d45cd7bbcd1c8e2f71118d39e461b28e
Polkovnik-Daniil <0205danik@mail.ru>
2023-05-25 18:30:56 +0300

    update visual homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,24 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BOOKS_ROUTE } from "../utils/consts";
 import "./Home.css";
+import { useNavigate } from "react-router-dom";
 
 export default function Home() {
+    const navigate = useNavigate();
 
     return (
-        <div>HomePage</div>
+        <div class="d-flex align-items-center justify-content-center vh-100">
+            <div class="text-center">
+                <h1 class="display-1 fw-bold text-white">Library</h1>
+                <p class="fs-3 text-white" > <span class="text-danger">Welcome</span> to the Library Data Application.</p>
+                <p class="lead">
+                    This is a small but interesting application for work, we are glad to everyone who uses it
+                </p>
+                <a class="btn btn-primary" onClick={() => {
+                    navigate(BOOKS_ROUTE);
+                    return;
+                }}>Go Books</a>
+            </div>
+        </div>
     );
 }
